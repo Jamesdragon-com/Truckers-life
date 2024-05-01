95a87b5fc5c5e9740e2bb8f2824e06c4a6eabf2a
meftah <majidabdlatif1@gmail.com>
2024-05-01 18:58:07 +0100

    Update s_main_menu.lua

--- main_menu/s_main_menu.lua
+++ main_menu/s_main_menu.lua
@@ -11,7 +11,8 @@ addEventHandler("onServerSendMessage", root, onServerSendMessage)
 
 function spawn_veh(x,y,z)
 local my_veh = createVehicle (403,x, y, z, 0, 0, 90)
-local my_veh_trailer = createVehicle (584,-56.34701, -1138.74963, 1.5, 0, 0, 90)
+local my_veh_trailer = createVehicle (450,-56.34701, -1138.74963, 1.5, 0, 0, 90)
+setVehicleVariant (my_veh_trailer, 0)
 attachTrailerToVehicle ( my_veh, my_veh_trailer )
 warpPedIntoVehicle ( source, my_veh,0 ) 
 end
